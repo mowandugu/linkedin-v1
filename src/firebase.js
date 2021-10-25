0b3e040e87fd35f4d463a1708ec9ad204b75ce5a
Mark Wandugu <mowandugu@Marks-MacBook-Pro.local>
2021-10-25 11:14:45 +0300

    added environmental variables

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import firebase from 'firebase'
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBSnY4JK4-8WpZJF4OozVcfoXZfB-xNjGM",
-    authDomain: "linkedin-app-ab3f7.firebaseapp.com",
-    projectId: "linkedin-app-ab3f7",
-    storageBucket: "linkedin-app-ab3f7.appspot.com",
-    messagingSenderId: "500406774202",
-    appId: "1:500406774202:web:3ec23ff86c08ae1c49d583",
-    measurementId: "G-ZN6ML4CFSJ"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
